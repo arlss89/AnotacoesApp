bff4d86044d2713076afe608d25646bc798943b8
Alyson <arlss89@gmail.com>
2021-03-16 22:45:13 -0300

    second commit - implementing basic functions for shared preferences.

---
+++ app/src/main/java/com/example/anotacoesapp/AnotacaoPreferencias.java
@@ -0,0 +1,34 @@
+package com.example.anotacoesapp;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.widget.Toast;
+
+public class AnotacaoPreferencias {
+
+    private Context context;
+    private SharedPreferences preferences;
+    private static final String NOME_ARQUIVO = "Anotacao.preferencia";
+    private static final String CHAVE_NOME = "nome";
+
+    private SharedPreferences.Editor editor;
+
+
+    public AnotacaoPreferencias(Context c) {
+        this.context = c;
+        preferences = context.getSharedPreferences(NOME_ARQUIVO, 0);
+        editor = preferences.edit();
+    }
+
+    public void salvarAnotacao(String anotacao){
+
+        editor.putString(CHAVE_NOME, anotacao);
+        editor.commit();
+
+    }
+
+    public String recuperarAnotacao(){
+        return preferences.getString(CHAVE_NOME, "Faça sua anotação!");
+    }
+
+}
